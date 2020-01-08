fc60b37cc8e7857557aa2c6a164ae0f02a588319
Presley <presley@pop-os.localdomain>
2020-01-08 20:40:15 +0800

    route update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Sidebar from "./components/sideBar";
 import About from "./components/about";
 import Education from "./components/education";
@@ -12,38 +12,45 @@ import "./App.css";
 
 const App = props => {
   return (
-    <Router>
-      <React.Fragment>
-        <Sidebar data={props.data} />
-        <div className="container-fluid p-0">
-          <Switch>
-            <Route
-              path="/about"
-              exact
-              component={() => <About data={props.data.about} />}
-            />
-            <Route
-              path="/education"
-              component={() => <Education data={props.data.education} />}
-            />
-            <Route
-              path="/experience"
-              component={() => <Experience data={props.data.experience} />}
-            />
-            <Route
-              path="/interests"
-              component={() => <Interest data={props.data.interests} />}
-            />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route
-              path="/skills"
-              component={() => <Skill data={props.data.skills} />}
-            />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>
+    <React.Fragment>
+      <Sidebar data={props.data} />
+      <div className="container-fluid p-0">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => <About data={props.data.about} />}
+          />
+          <Route
+            exact
+            path="/about"
+            component={() => <About data={props.data.about} />}
+          />
+          <Route
+            exact
+            path="/education"
+            component={() => <Education data={props.data.education} />}
+          />
+          <Route
+            exact
+            path="/experience"
+            component={() => <Experience data={props.data.experience} />}
+          />
+          <Route
+            exact
+            path="/interests"
+            component={() => <Interest data={props.data.interests} />}
+          />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route
+            exact
+            path="/skills"
+            component={() => <Skill data={props.data.skills} />}
+          />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Data from "./data";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App data={Data} />, document.getElementById("root"));
+ReactDOM.render(
+  <HashRouter basename={process.env.REACT_APP_ROUTER_BASE || ""}>
+    <App data={Data} />
+  </HashRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
